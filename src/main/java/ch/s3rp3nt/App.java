cb776c8c7c0dd3f1e2171010200e0e9b289ad90a
jvankocherhans <kocherhansjvan@gmail.com>
2023-10-06 08:57:24 +0200

    generated Java MVN project

---
+++ src/main/java/ch/s3rp3nt/App.java
@@ -0,0 +1,13 @@
+package ch.s3rp3nt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
